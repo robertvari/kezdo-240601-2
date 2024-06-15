29034ffe47db14980c4e0aefcd1b6c831ba5bb21
Robert Vari <mail.robertvari@gmail.com>
2024-06-15 16:25:21 +0200

    update

--- 11_libraries.py
+++ 11_libraries.py
@@ -1,4 +1,9 @@
 from my_functions import math_functions as mf
+# Explicit import
+# from my_functions.math_functions import add_numbers, divide_numbers, multiply_numbers
+
+# import all
+# from my_functions.math_functions import *
 
 result = mf.add_numbers(10, 20)
 result = mf.divide_numbers(result, 20)
