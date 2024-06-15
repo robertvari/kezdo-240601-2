98defeebc230e423f48fcb7e9aa5634c919079d2
Robert Vari <mail.robertvari@gmail.com>
2024-06-15 14:37:56 +0200

    update

---
+++ 03_multiple_params.py
@@ -0,0 +1,6 @@
+def say_hello(name, email, address):
+    print(f"Hello {name}.")
+    print(f"You live in {address}")
+    print(f"Your email is {email}")
+
+say_hello(email="csaba@gmail.com", address="Budapest", name="Csaba")
