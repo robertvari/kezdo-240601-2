5c9219b35e689ba181012825b846e4e5a256297e
Robert Vari <mail.robertvari@gmail.com>
2024-06-16 17:25:15 +0200

    update

---
+++ 21_job_queue.py
@@ -0,0 +1,13 @@
+file_list = [
+    "image_0001.exr",
+    "image_0002.exr",
+    "image_0003.exr",
+    "image_0004.exr",
+    "image_0005.exr",
+    "image_0006.exr",
+    "image_0007.exr",
+    "image_0008.exr",
+    "image_0009.exr",
+    "image_0010.exr",
+    "image_0011.exr",
+]
