1314e00269d5fcab7273ef5d3df68d3105023243
Robert Vari <mail.robertvari@gmail.com>
2024-06-16 14:12:25 +0200

    update

---
+++ 12_lambda_functions.py
@@ -0,0 +1,8 @@
+add_numbers = lambda a, b: a + b
+divide_numbers = lambda a, b: a / b
+multiply_numbers = lambda a, b: a * b
+
+
+print(add_numbers(10, 20))
+print(divide_numbers(10, 20))
+print(multiply_numbers(10, 20))
