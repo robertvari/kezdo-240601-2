2d8ed7326bb283003bfac4cb2ea04b26ba499a1c
Robert Vari <mail.robertvari@gmail.com>
2024-06-16 18:11:05 +0200

    update

--- poster_downloader.py
+++ poster_downloader.py
@@ -2,12 +2,12 @@ import tmdbsimple as tmdb
 import requests, os
 tmdb.API_KEY = "83cbec0139273280b9a3f8ebc9e35ca9"
 
-poster_folder = r"movie_posters"
+POSTER_FOLDER = r"movie_posters"
 POSTER_ROOT_PATH = "https://image.tmdb.org/t/p/w300"
 
 def main():
-    if not os.path.exists(poster_folder):
-        os.makedirs(poster_folder)
+    if not os.path.exists(POSTER_FOLDER):
+        os.makedirs(POSTER_FOLDER)
 
     movies = get_movies()
 
@@ -24,8 +24,8 @@ def get_poster(movie_data):
     poster_path = f'{POSTER_ROOT_PATH}{movie_data["poster_path"]}'
     image_name = poster_path.split("/")[-1]
     print(f"Downloading {image_name}")
-    
-    file_path = os.path.join(poster_folder, image_name)
+
+    file_path = os.path.join(POSTER_FOLDER, image_name)
     
     img_data = requests.get(poster_path).content
     with open(file_path, "wb") as f:
