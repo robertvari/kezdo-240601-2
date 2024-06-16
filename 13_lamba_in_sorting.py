34db5bea13ab42b7b9e7eb45fe9ca29e89931d10
Robert Vari <mail.robertvari@gmail.com>
2024-06-16 14:27:49 +0200

    update

--- 13_lamba_in_sorting.py
+++ 13_lamba_in_sorting.py
@@ -1,1 +1,8 @@
 my_friends = ["Vári Róbert", "Kiss Balázs", "Kovács Elemér", "Tóth Krisztina", "Kiss Csaba Géza"]
+
+default_sorting = sorted(my_friends)
+print(default_sorting)
+
+
+modified_sorting = sorted(my_friends, key=lambda name: name.split()[-1])
+print(modified_sorting)
